9bb1c703fe4db5eb4af8e3dc2f7815e022ab98bc
SrujankumarGurram <116895927+SrujankumarGurram@users.noreply.github.com>
2022-12-02 15:06:57 +0530

    Create Patient.js

---
+++ Login/Patient.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import EntryPage from './EntryPage'
+import './Login.scss'
+const Patient = () => {
+    return (
+        <EntryPage/>
+      )
+}
+
+export default Patient
