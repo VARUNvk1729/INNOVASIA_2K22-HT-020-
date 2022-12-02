293bbb9fffae6f6b426c664e58a8d62451ece170
SrujankumarGurram <116895927+SrujankumarGurram@users.noreply.github.com>
2022-12-02 15:06:34 +0530

    Create Labadmin.js

---
+++ Login/Labadmin.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import EntryPage from './EntryPage'
+import './Login.scss'
+const Labadmin = () => {
+    return (
+        <EntryPage/>
+      )
+};
+
+export default Labadmin
